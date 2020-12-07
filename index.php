2e7eb65415b03fda281f00777e60a19ea70e5548
Pikachu! <44121227+BethanyJep@users.noreply.github.com>
2020-12-07 12:14:35 +0300

    first commit - initial template

---
+++ index.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
+		
+		<title>Band Name</title>
+		<!-- Loading third party fonts -->
+		<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900" rel="stylesheet" type="text/css">
+		<link href="fonts/font-awesome.min.css" rel="stylesheet" type="text/css">
+		<!-- Loading main css file -->
+		<link rel="stylesheet" href="style.css">
+		
+		<!--[if lt IE 9]>
+		<script src="js/ie-support/html5.js"></script>
+		<script src="js/ie-support/respond.js"></script>
+		<![endif]-->
+
+	</head>
+
+	<body class="header-collapse">
+		
+		<div id="site-content">
+			<header class="site-header">
+				<div class="container">
+					<a href="index.html" id="branding">
+						<img src="dummy/logo.png" alt="Site Title">
+						<small class="site-description">Only Music</small>
+					</a> <!-- #branding -->
+					
+					<nav class="main-navigation">
+						<button type="button" class="toggle-menu"><i class="fa fa-bars"></i></button>
+						<ul class="menu">
+							<li class="menu-item current-menu-item"><a href="index.html">Home</a></li>
+							<li class="menu-item"><a href="about.html">About</a></li>
+							<li class="menu-item"><a href="gallery.html">Projects</a></li>
+							<li class="menu-item"><a href="blog.html">Blog</a></li>
+							<li class="menu-item"><a href="contact.html">Contact</a></li>
+						</ul> <!-- .menu -->
+					</nav> <!-- .main-navigation -->
+					<div class="mobile-menu"></div>
+				</div>
+			</header> <!-- .site-header -->
+			
+			<div class="hero">
+				<div class="slider">
+					<ul class="slides">
+						<li class="lazy-bg" data-background="dummy/slide-4.jpg">
+							<div class="container">
+								<h2 class="slide-title">Rotaract Club of JKUAT</h2>
+								<h3 class="slide-subtitle">Rotary Connects the world</h3>
+								<p class="slide-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>Fugiat aliquid minus nemo sed est.</p>
+
+								<a href="#" class="button cut-corner">Read More</a>
+							</div>
+						</li>
+						<li class="lazy-bg" data-background="dummy/slide-5.png">
+							<div class="container">
+									<h2 class="slide-title">Rotaract Club of JKUAT</h2>
+								<h3 class="slide-subtitle">Rotary Connects the world</h3>
+								<p class="slide-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>Fugiat aliquid minus nemo sed est.</p>
+
+
+									<a href="#" class="button cut-corner">Read More</a>
+							</div>
+						</li>
+						<li class="lazy-bg" data-background="dummy/join1.jpg">
+							<div class="container">
+								<h2 class="slide-title">Rotaract Club of JKUAT</h2>
+								<h3 class="slide-subtitle">Rotary Connects the world</h3>
+								<p class="slide-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>Fugiat aliquid minus nemo sed est.</p>
+
+
+									<a href="#" class="button cut-corner">Read More</a>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+
+				<div class="fullwidth-block why-chooseus-section">
+					<div class="container">
+						<h2 class="section-title">Why join Rotaract?</h2>
+
+						<div class="row">
+							<div class="col-md-4">
+								<div class="feature">
+									<figure class="cut-corner">
+										<img src="dummy/join1.jpg" alt="" height="230">
+									</figure>
+									<h3 class="feature-title">Proffesional and Leadership develpment</h3>
+									<p>Rotaract has occasionall meating that will benefit for professional and leadeship develpment.</p>
+								</div> <!-- .feature -->
+							</div>
+							<div class="col-md-4">
+								<div class="feature">
+									<figure class="cut-corner">
+										<img src="dummy/join2.jpg" alt="">
+									</figure>
+									<h3 class="feature-title">Community Service</h3>
+									<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+								</div> <!-- .feature -->
+							</div>
+							<div class="col-md-4">
+								<div class="feature">
+									<figure class="cut-corner">
+										<img src="dummy/join3.jpg" alt="">
+									</figure>
+									<h3 class="feature-title">Fun</h3>
+									<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+								</div> <!-- .feature -->
+							</div>
+						</div>
+					</div> <!-- .container -->
+				</div> <!-- .why-chooseus-section -->
+			
+				<div class="fullwidth-block upcoming-event-section" data-bg-color="">
+					<div class="container">
+						<header class="section-header">
+							<h2 class="section-title">Upcoming events/projects</h2>
+
+							<div class="event-nav">
+								<a class="prev" id="event-prev" href="#"><i class="fa fa-caret-left"></i></a>
+	    						<a class="next" id="event-next" href="#"><i class="fa fa-caret-right"></i></a>
+							</div> <!-- .event-nav -->
+
+						</header> <!-- .section-header -->
+						<div class="event-carousel">
+							
+							<div class="event">
+								<div class="entry-date">
+									<div class="date">23</div>
+									<span class="month">Jun</span>
+								</div>
+								<h2 class="entry-title"><a href="#">Sed et persipiatis unde omnis iste natus</a></h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur</p>
+							</div> <!-- .event -->
+							
+							
+							<div class="event">
+								<div class="entry-date">
+									<div class="date">23</div>
+									<span class="month">Jun</span>
+								</div>
+								<h2 class="entry-title"><a href="#">Sed et persipiatis unde omnis iste natus</a></h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur</p>
+							</div> <!-- .event -->
+							
+							
+							<div class="event">
+								<div class="entry-date">
+									<div class="date">23</div>
+									<span class="month">Jun</span>
+								</div>
+								<h2 class="entry-title"><a href="#">Sed et persipiatis unde omnis iste natus</a></h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur</p>
+							</div> <!-- .event -->
+							
+							
+							<div class="event">
+								<div class="entry-date">
+									<div class="date">23</div>
+									<span class="month">Jun</span>
+								</div>
+								<h2 class="entry-title"><a href="#">Sed et persipiatis unde omnis iste natus</a></h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur </p>
+							</div> <!-- .event -->
+							
+							
+							<div class="event">
+								<div class="entry-date">
+									<div class="date">23</div>
+									<span class="month">Jun</span>
+								</div>
+								<h2 class="entry-title"><a href="#">Sed et persipiatis unde omnis iste natus</a></h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur </p>
+							</div> <!-- .event -->
+							
+						</div> <!-- .event-carousel -->
+					</div> <!-- .container -->
+				</div> <!-- .upcoming-event-section -->
+
+				
+			</main> <!-- .main-content -->
+
+
+			<footer class="site-footer">
+				
+			</footer> <!-- .site-footer -->
+
+		</div> <!-- #site-content -->
+
+		<script src="js/jquery-1.11.1.min.js"></script>		
+		<script src="js/plugins.js"></script>
+		<script src="js/app.js"></script>
+		
+	</body>
+
+</html>
